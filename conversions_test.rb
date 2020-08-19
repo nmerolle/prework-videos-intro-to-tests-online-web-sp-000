e5e0fc53673d7a62564563d8bb01ef5c3c102eb4
voluble-navigator-8704 <nmerolle11@hotmail.com>
2020-08-19 11:18:48 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -5,7 +5,7 @@ answer = ounces_to_grams(0)
 if answer == 0.0
   puts "Passed!"
 else
-  puts "Test failed: got #{answer} instead"
+  puts "Test failed: got #{answer} instead."
 end
 
 puts "Given 1, it should return 28.3495"
@@ -13,5 +13,5 @@ answer = ounces_to_grams(1)
 if answer == 28.3495
   puts "Passed!"
 else
-  puts "Test failed: got #{answer} instead"
+  puts "Test failed: got #{answer} instead."
 end
